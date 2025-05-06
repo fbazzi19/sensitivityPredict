9c898597ddbcaa4fc01831c3e6f241788faa038b
fbazzi19 <fbazzi19@gmail.com>
2025-05-06 16:10:12 +0200

    Account for commas in drug names

---
+++ helperScripts/line_parser.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+
+def parse_line(line):
+    last_comma=line.rfind(",")
+    return line[0:last_comma], line[last_comma+1:len(line)]
+
+if __name__ == "__main__":
+    line = sys.argv[1]
+    try:
+        drug_name, drug_id = parse_line(line)
+        print(drug_name+"\n"+drug_id)
+    except ValueError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+
